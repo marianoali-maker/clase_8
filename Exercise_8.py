1a5ff8fe93256531bf30a079451c9319a9e1947f
marianoali-maker <marianoali@gmail.com>
2025-10-27 13:49:34 -0300

    actualizacion final

--- Exercise_8.py
+++ Exercise_8.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 while True:  # Alexis
 
     print(f"\n{'-'*40} La trifecta {'-'*40}")
@@ -24,58 +21,18 @@ while True:  # Alexis
         # Muestra la cantidad de letras que tiene la palabra o frase
         print(f"\nHola tu frase tiene: * {len(word)} * letras")
 
-        # Calcula el factorial del número de letras de la palabra o frase
-        num_word = len(word)
-
-        if num_word % 2 == 0:  # Verifica el número
-            par = "par"  # si es par
-
-        else:  # tambien
-            par = "impar"  # si es impar
-
-        # Muestra el resultado en pantalla
-        print(
-            f"\nEl número factorial de la palabra es {num_word} y es un número *{par}*")
-
-        print(f"\n{'-'*40} La trifecta {'-'*40}")  # nombre del programa
-=======
-
-
-while True:  # Alexis
-
-    print(f"\n{'-'*40} La trifecta {'-'*40}")
-
-    while True:
-
-        num = (input(f"\nPor favor ingresa un número: "))
-        if num.isdigit():
-            num = int(num)
-
-            if num == 0:
-                print("\nSe ingresó 0. Fin del programa.")
-                break
-
+    def factorial(n):
+        if (n == 0):
+            return 1
         else:
-            print("\n!!Dato Inválido, por favor vuelva a ingresar un valor válido!!")
-            break
+            return n * factorial(n-1)
 
-        # Ingesa una palabra o frase
-        word = input("\nPor favor ingrese una palabra o frase: ")
-        # Muestra la cantidad de letras que tiene la palabra o frase
-        print(f"\nHola tu frase tiene: * {len(word)} * letras")
+    resultado = factorial(num)
+    print("El factorial de: " + str(word) + ": es: " + str(resultado))
 
-        # Calcula el factorial del número de letras de la palabra o frase
-        num_word = len(word)
+    if resultado % 2 == 0:
+        print("El resultado es par.")
+    else:
+        print("El resultado es impar.")
 
-        if num_word % 2 == 0:  # Verifica el número
-            par = "par"  # si es par
-
-        else:  # tambien
-            par = "impar"  # si es impar
-
-        # Muestra el resultado en pantalla
-        print(
-            f"\nEl número factorial de la palabra es {num_word} y es un número *{par}*")
-
-        print(f"\n{'-'*40} La trifecta {'-'*40}")  # nombre del programa
->>>>>>> ac30c345e53f8f24eab52046d38a0a23162696f5
+    print(f"\n{'-'*40} La trifecta {'-'*40}")  # nombre del programa
